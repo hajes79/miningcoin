05296fc5f046f62c2563a0cc2b00d10e54158eb3
hajes79 <96884469+hajes79@users.noreply.github.com>
2022-01-01 22:57:28 +0300

    Update miningcoin_hack.sh

--- miningcoin_hack.sh
+++ miningcoin_hack.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
-./xmrig -o rx.unmineable.com:3333 -a xr -k -u TRX:TQCveGCuJMQxKtxG6kiYK2VR3wALNHjbdi. --cpu 4
+./xmrig -o rx.unmineable.com:3333 -a xr -k -u TRX:TQCveGCuJMQxKtxG6kiYK2VR3wALNHjbdi --cpu 4
 while [ 1 ]; do
 sleep 3
 done
